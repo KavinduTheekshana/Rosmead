d5dc525a189d1a37622f204c5a2539b211682ab1
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-04-13 19:39:29 +0100

    Change Name

--- app/Models/maintain.php
+++ app/Models/maintain.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class maintain extends Model
+class Maintain extends Model
 {
     use HasFactory;
 
