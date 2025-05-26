e6d6155b71628d32508c89b8953fa19b50fe95d6
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-05-26 23:31:05 +0100

    Darkmode logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandLogo(asset('BrilliantHome.svg'))
+            ->darkModeBrandLogo(asset('BrilliantHomeLight.svg')) // Logo for dark mode
             ->brandLogoHeight('2rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
